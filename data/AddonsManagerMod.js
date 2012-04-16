7dc2915f7c70611afc41f7171861fb920c339c58
brianking <brian@kingsonline.net>
2012-04-16 19:31:37 +0200

    After misunderstanding the spec, switching to an implementation that requires no panel but just a button to open Collections and a link to the blog post on retirement

--- data/AddonsManagerMod.js
+++ data/AddonsManagerMod.js
@@ -1,108 +1,65 @@
 CollectionsController = {};
 
 CollectionsController.COLLECTIONS_URL = "https://addons.mozilla.org/collections/";
+CollectionsController.RETIREMENT_URL = "http://blog.mozilla.org/addons/2012/03/07/add-on-collector-retiremen/";
 
 self.port.on("collector_init", function(data) {
-    CollectionsController.loadViewPanel();
-    CollectionsController.loadView();
+    CollectionsController.loadButton();
+    CollectionsController.loadRetirement();
 });
 
-self.port.on("collector_refresh", function(data) {
-    CollectionsController.onViewChanged();
+self.port.on("collector_disable", function(data) {
+    CollectionsController.cleanUp();
 });
 
-CollectionsController.loadViewPanel = function()
+CollectionsController.loadButton = function()
 {
-    /*
-        <browser id="collections-browser" type="content" flex="1"
-                       disablehistory="true" homepage="about:blank"/>
-    */
-    var viewport = document.getElementById("view-port");
-    var collectionsBox = document.createElement("deck");
-    collectionsBox.setAttribute("id", "collections-view");
-    collectionsBox.setAttribute("flex", "1");
-    collectionsBox.setAttribute("class", "view-pane");
-    var collectionsBrowser = document.createElement("browser");
-    collectionsBrowser.setAttribute("id", "collections-browser");
-    collectionsBrowser.setAttribute("type", "content");
-    collectionsBrowser.setAttribute("flex", "1");
-    collectionsBrowser.setAttribute("disablehistory", "true");
-    collectionsBrowser.setAttribute("homepage", "about:blank");
-    collectionsBox.appendChild(collectionsBrowser);
-    viewport.appendChild(collectionsBox);
-}
-
-CollectionsController.loadView = function()
-{
-    /*
-        <richlistitem id="bandwagon-collections-view" value="addons://collectionsnew/"
-            class="category"
-            name="Collections" />
-    */
+    // XX TODO Localise strings
     var cats = document.getElementById("categories");
-    var collsItems = document.createElement("richlistitem");
-    collsItems.setAttribute("id", "collections-view-item");
-    collsItems.setAttribute("value", "addons://collectionsnew/");
-    collsItems.setAttribute("class", "category");
-    collsItems.setAttribute("name", "Collections");
+    var collsButton = document.createElement("button");
+    collsButton.setAttribute("id", "collections-button");
+    collsButton.setAttribute("class", "category header-button");
+    collsButton.setAttribute("label", "Collections");
+    collsButton.setAttribute("name", "Collections");
+    collsButton.addEventListener("click", function() {
+        openURL(CollectionsController.COLLECTIONS_URL);
+    }, true);
 
-    gViewController.viewObjects["collectionsnew"] = 
-    {
-        node: null,
-        enabled: true,
-        loaded: false,
-        _browser: null,
-        homepageURL: "https://addons.mozilla.org/collections/",
-        _listBox: null,
-
-        initialize: function() {
-            this.node = document.getElementById("collections-view");
-            this._browser = document.getElementById("collections-browser");
-        },
-
-        show: function(aParam, aRequest, aState, aIsRefresh) {
-            if (this.loaded)
-                return;
-
-            this.loaded = true;
-
-            this._loadURL(this.homepageURL, false);
-
-            gViewController.updateCommands();
-            gViewController.notifyViewChanged();
-        },
-
-        _loadURL: function(aURL, aKeepHistory, aCallback) {
-            if (this._browser.currentURI.spec == aURL) {
-              if (aCallback)
-                aCallback();
-              return;
-            }
-
-            if (aCallback)
-              this._loadListeners.push(aCallback);
-
-            var flags = 0;
-            if (!aKeepHistory)
-              flags |= Ci.nsIWebNavigation.LOAD_FLAGS_REPLACE_HISTORY;
-
-            this._browser.loadURIWithFlags(aURL, flags);
-        },
-
-        hide: function() { },
-
-        getSelectedAddon: function() null
-    };
-
-    cats.appendChild(collsItems);
-    gViewController.viewObjects["collectionsnew"].initialize();
+    cats.appendChild(collsButton);
 }
 
-CollectionsController.onViewChanged = function()
+CollectionsController.loadRetirement = function()
 {
-    // XX TODO Determine if anything needs to be done here
+    // XX TODO Localise strings
+    var cats = document.getElementById("categories");
+    var retBox = document.createElement("vbox");
+    retBox.setAttribute("id", "retirement-box");
+    retBox.setAttribute("align", "start");
+    // XX TOOD Figure out how to wrap the following text
+    var retText = document.createElement("description");
+    retText.setAttribute("id", "retirement-text");
+    retText.setAttribute("flex", "1");
+    retText.setAttribute("crop", "end");
+    retText.textContent = "We retired the Add-on Collector"
+    var retButton = document.createElement("button");
+    retButton.setAttribute("id", "retirement-button");
+    retButton.setAttribute("class", "button-link");
+    retButton.setAttribute("label", "Find out why >");
+    retButton.addEventListener("click", function() {
+        openURL(CollectionsController.RETIREMENT_URL);
+    }, true);
+
+    retBox.appendChild(retText);
+    retBox.appendChild(retButton);
+    cats.appendChild(retBox);
 }
 
-//Services.obs.addObserver(init, "EM-loaded", false);
-//document.addEventListener("ViewChanged", CollectionsController.onViewChanged, true);
+CollectionsController.cleanUp = function()
+{
+    var cats = document.getElementById("categories");
+    var collsButton = document.getElementById("collections-button");
+    var retBox = document.getElementById("retirement-box");
 
+    cats.removeChild(collsButton);
+    cats.removeChild(retBox);
+}
